72369fe89f8cdd9ae400d3f06c96b9cfa0954639
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-14 22:22:37 +0200

    send help

---
+++ %_of_overall_cost_1-3_upgraded.py
@@ -0,0 +1,44 @@
+from main import units, town_colors
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+tiers = (1, 7)
+state = "upgraded"
+units = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]} & state == "{state}"')
+units["max_gold_cost"] = units.max_growth * units.gold_cost
+
+t = pd.DataFrame(index=[town for town in town_colors.keys() if town != 'neutral'])
+t["overall_gold_cost"] = units.groupby(["fraction"])['max_gold_cost'].sum()
+
+tiers = (1, 3)
+units = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]}')
+t["gold_cost13"] = units.groupby(["fraction"])['max_gold_cost'].sum()
+t["%"] = t.gold_cost13 / t.overall_gold_cost * 100
+t = t.sort_values(by="%", ascending=False)
+plt.figure(figsize=(18.5, 10.5))
+plt.title("% of overall cost of upgraded units tiers 1-3")
+
+for town in t.index.values:
+    plt.bar(x=town, height=t.loc[town]["%"], color=town_colors[town])
+
+#x=list(t1.index.values)
+#y=t1["max_gold_cost"]
+#print(x, y)
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
