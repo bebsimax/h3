ae5d17da876fec7c40a14968763288d463676d58
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-16 11:21:08 +0200

    ghetto, but works

--- units_damage_per_gold_up_neutral.py
+++ units_damage_per_gold_up_neutral.py
@@ -1,22 +1,43 @@
-from main import units, town_colors
-from operator import itemgetter
+from main import units, town_colors, legend_elements, chart_data_path
+from common import average_damage
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-
-
+chart_name = "sorted by damage"
 set_gold = 5000
+chart_path = os.path.join(chart_data_path, f"{chart_name}.png")
 states = ['upgraded', 'neutral']
 def set_avarage_damage(row):
-    if row["gold_cost"] == 0:
-        return 0
     if "twice" in row["special"]:
-        return ((row["damage_min"] * 0.05 * row["attack"]) + (row["damage_max"] * 0.05 * row["attack"])) /row["gold_cost"] * set_gold
+        return average_damage.av_dmg(row["damage_min"], row["damage_max"], row["attack"]) * 2 * (set_gold/row["gold_cost"])
     else:
-        return ((row["damage_min"] * 0.05 * row["attack"]) + (row["damage_max"] * 0.05 * row["attack"])) /2 /row["gold_cost"] * set_gold
+        return average_damage.av_dmg(row["damage_min"], row["damage_max"], row["attack"]) * (set_gold/row["gold_cost"])
+
 units = units.query(f'state == "{states[0]}" or state == "{states[1]}"')
-units = units.assign(avarage_damage=units.apply(set_avarage_damage, axis=1))
-units = units.sort_values(by="avarage_damage", ascending=False)
-units.plot(x=0, y=16, kind='barh', figsize=(16.5, 8.5), ylabel=f"damage per {set_gold} gold", xlabel="unit", legend=False,
-       title=f'upgraded and neutral units damage per {set_gold} gold')
-plt.show()
+units = units.assign(average_damage=units.apply(set_avarage_damage, axis=1))
+units = units.sort_values(by="average_damage", ascending=False)
+
+
+def plot_with_colors(data, y, colors, x=0):
+    if x == 0:
+        for i in range(len(data)):
+            plt.barh(y=data.iloc[i]["name"], width=data.iloc[i][y], color=colors[data.iloc[i]["fraction"]])
+        else:
+            plt.legend(handles=legend_elements)
+            plt.xlabel('Average damage per 5000 gold vs 0 defense target')
+            plt.yticks(fontsize=8)
+            plt.legend(handles=legend_elements)
+            fig = plt.gcf()
+            fig.set_size_inches(18, 10)
+            plt.savefig(fname=chart_path, dpi=350, format="png")
+            plt.show()
+
+
+plot_with_colors(units, "average_damage", town_colors)
+
+
+
+
+
+
