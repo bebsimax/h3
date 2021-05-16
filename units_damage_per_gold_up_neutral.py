ab04e2b6fbdd3e7f4de6f2c2a7dd7e960ec807c8
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-16 16:34:41 +0200

    better, units groupby

--- units_damage_per_gold_up_neutral.py
+++ units_damage_per_gold_up_neutral.py
@@ -16,26 +16,18 @@ def set_avarage_damage(row):
 
 units = units.query(f'state == "{states[0]}" or state == "{states[1]}"')
 units = units.assign(average_damage=units.apply(set_avarage_damage, axis=1))
-units = units.sort_values(by="average_damage", ascending=False)
+#units = units.sort_values(by="average_damage", ascending=False)
 
-
-def plot_with_colors(data, y, colors, x=0):
-    if x == 0:
-        for i in range(len(data)):
-            plt.barh(y=data.iloc[i]["name"], width=data.iloc[i][y], color=colors[data.iloc[i]["fraction"]])
-        else:
-            plt.legend(handles=legend_elements)
-            plt.xlabel('Average damage per 5000 gold vs 0 defense target')
-            plt.yticks(fontsize=8)
-            plt.legend(handles=legend_elements)
-            fig = plt.gcf()
-            fig.set_size_inches(18, 10)
-            plt.savefig(fname=chart_path, dpi=350, format="png")
-            plt.show()
-
-
-plot_with_colors(units, "average_damage", town_colors)
-
+for (name, damage), group in units.groupby(["fraction", "average_damage"]):
+    plt.barh(y=group["name"], width=group["average_damage"], color=town_colors[name])
+else:
+    plt.legend(handles=legend_elements)
+    plt.xlabel('Average damage per 5000 gold vs 0 defense target')
+    plt.yticks(fontsize=8)
+    plt.legend(handles=legend_elements)
+    fig = plt.gcf()
+    fig.set_size_inches(18, 10)
+    plt.show()
 
 
 
