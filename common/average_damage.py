5c861102e06af27cf17a9d0f5aba661ce367edfd
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-27 19:35:39 +0200

    included double strike, double shoots, ferocity and death's blow

--- common/average_damage.py
+++ common/average_damage.py
@@ -1,9 +1,15 @@
-def av_dmg(damage_min, damage_max, attack, defense=0):
+def av_dmg(damage_min, damage_max, attack, special, defense=0):
     ADD = attack - defense
+    if "Death blow" in special:
+        damage_min = 1.2 * damage_min
     if ADD >= 0:
         d_min = damage_min * (1 + 0.05*ADD)
         d_max = damage_max * (1 + 0.05*ADD)
     else:
         d_min = damage_min * (1 - 0.025*ADD)
         d_max = damage_max * (1 - 0.025*ADD)
-    return (d_min+d_max)/2
+    avg = (d_min+d_max)/2
+    if "twice" in special or "Ferocity" in special:
+        return avg*2
+    else:
+        return avg
