c2beb82b4532b52f5bba2013fca627ac186784a4
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-28 13:04:10 +0200

    changed function name

--- common/average_damage.py
+++ common/average_damage.py
@@ -1,14 +1,19 @@
-def av_dmg(damage_min, damage_max, attack, special, defense=0):
+def avg_dmg(damage_min, damage_max, attack, special, defense=0):
+
     ADD = attack - defense
+
     if "Death blow" in special:
         damage_min = 1.2 * damage_min
+
     if ADD >= 0:
         d_min = damage_min * (1 + 0.05*ADD)
         d_max = damage_max * (1 + 0.05*ADD)
     else:
         d_min = damage_min * (1 - 0.025*ADD)
         d_max = damage_max * (1 - 0.025*ADD)
+
     avg = (d_min+d_max)/2
+
     if "twice" in special or "Ferocity" in special:
         return avg*2
     else:
