ff095874497e4d44c6c99c46ed4ac2eea066e2b1
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-28 09:43:29 +0200

    added chart path to save to

--- overall_cost_4-5_upgraded.py
+++ overall_cost_4-5_upgraded.py
@@ -1,12 +1,17 @@
-from main import units, town_colors
+from main import units, town_colors, chart_data_path
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 
 tiers = (4, 5)
 state = "upgraded"
+title = 'gold cost of maximum growth for each town, units tiers 4-5'
+
+
 units = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]} & state == "{state}"')
 units["max_gold_cost"] = units.max_growth * units.gold_cost
-
+print(units[units["fraction"] == 'rampart'])
 t = pd.DataFrame(index=[town for town in town_colors.keys() if town != 'neutral'])
 t["max_gold_cost"] = units.groupby(["fraction"])['max_gold_cost'].sum()
 
@@ -16,8 +21,9 @@ t = t.sort_values(by="max_gold_cost", ascending=False)
 indexes = t.index.values
 colors_in_order = [town_colors[town] for town in indexes]
 
-t.plot(y=0, kind='bar', color=colors_in_order, figsize=(16.5, 8.5), xlabel="towns", ylabel="gold", legend=False,
-       title='cost of units tiers 4-5 for each town max growth', ylim=(4_000, 9_000))
-
+chart = t.plot(y=0, kind='bar', color=colors_in_order, figsize=(16.5, 8.5), xlabel="towns", ylabel="gold", legend=False,
+       title=title, ylim=(3_000, 8_500))
+fig = chart.get_figure()
+fig.savefig(os.path.join(chart_data_path, title + '.png'))
 
 plt.show()
