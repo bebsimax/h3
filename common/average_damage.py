e141fa7fcda1f75bee39bca524aea0b35a12e75c
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-28 13:32:41 +0200

    added function for pandas series, need to merge them into one

--- common/average_damage.py
+++ common/average_damage.py
@@ -18,3 +18,29 @@ def avg_dmg(damage_min, damage_max, attack, special, defense=0):
         return avg*2
     else:
         return avg
+
+def avg_dmg_series(row):
+    damage_min = row["damage_min"]
+    damage_max = row["damage_max"]
+    attack = row["attack"]
+    special = row["special"]
+    defense = 0
+
+    ADD = attack - defense
+
+    if "Death blow" in special:
+        damage_min = 1.2 * damage_min
+
+    if ADD >= 0:
+        d_min = damage_min * (1 + 0.05 * ADD)
+        d_max = damage_max * (1 + 0.05 * ADD)
+    else:
+        d_min = damage_min * (1 - 0.025 * ADD)
+        d_max = damage_max * (1 - 0.025 * ADD)
+
+    avg = (d_min + d_max) / 2
+
+    if "twice" in special or "Ferocity" in special:
+        return avg * 2
+    else:
+        return avg
