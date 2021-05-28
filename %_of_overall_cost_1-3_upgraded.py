897498fe511a4b155bb08c1184656a4a3c822e62
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-28 10:14:20 +0200

    merged 2 charts

--- %_of_overall_cost_1-3_upgraded.py
+++ %_of_overall_cost_1-3_upgraded.py
@@ -7,28 +7,52 @@ import os
 
 tiers = (1, 7)
 state = "upgraded"
-title = '% of towns overall cost are units tiers 1-3'
+title_1 = '% of towns overall cost, units tiers 1-3'
+title_2 = 'gold cost of maximum growth for each town, units tiers 1-3'
+fig, axes = plt.subplots(nrows=2, ncols=1)
 
-
-
-units = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]} & state == "{state}" & name != "corsairs"')
-units["max_gold_cost"] = units.max_growth * units.gold_cost
+units_all_tiers = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]} & state == "{state}" & name != "corsairs"')
+units_all_tiers["max_gold_cost"] = units.max_growth * units.gold_cost
 t = pd.DataFrame(index=[town for town in town_colors.keys() if town != 'neutral'])
-t["overall_gold_cost"] = units.groupby(["fraction"])['max_gold_cost'].sum()
+t["overall_gold_cost"] = units_all_tiers.groupby(["fraction"])['max_gold_cost'].sum()
 
 tiers = (1, 3)
-units = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]}')
-t["gold_cost13"] = units.groupby(["fraction"])['max_gold_cost'].sum()
+units_tier_13 = units_all_tiers.query(f'tier >= {tiers[0]} & tier <= {tiers[1]}')
+t["gold_cost13"] = units_tier_13.groupby(["fraction"])['max_gold_cost'].sum()
 t["%"] = t.gold_cost13 / t.overall_gold_cost * 100
 t = t.sort_values(by="%", ascending=False)
 
 indexes = t.index.values
 colors_in_order = [town_colors[town] for town in indexes]
 
-chart = t.plot(y=2, kind='bar', color=colors_in_order, figsize=(16.5, 8.5), xlabel="towns", ylabel="%", legend=False,
-       title=title, ylim=(20, 35))
+chart = t.plot(ax=axes[0], y=2, kind='bar', color=colors_in_order, figsize=(18, 10), xlabel="towns", ylabel="%", legend=False,
+       title=title_1, ylim=(20, 35))
+
+
+tiers = (1, 3)
+state = "upgraded"
+title = "gold cost of maximum growth for each town, units tiers 1-3"
+
+units_tier_13_2 = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]} & state == "{state}" & name != "corsairs"')
+units_tier_13_2["max_gold_cost"] = units_tier_13_2.max_growth * units_tier_13_2.gold_cost
+
+t_2 = pd.DataFrame(index=[town for town in town_colors.keys() if town != 'neutral'])
+t_2["max_gold_cost"] = units_tier_13_2.groupby(["fraction"])['max_gold_cost'].sum()
+
+
+t_2 = t_2.sort_values(by="max_gold_cost", ascending=False)
+
+indexes = t_2.index.values
+colors_in_order = [town_colors[town] for town in indexes]
+
+chart_2 = t_2.plot(ax=axes[1], y=0, kind='bar', color=colors_in_order, figsize=(18, 10), xlabel="towns", ylabel="gold", legend=False,
+                   ylim=(6_000, 10_000))
+
+
+
+
 fig = chart.get_figure()
-fig.savefig(os.path.join(chart_data_path, title + '.png'))
+fig.savefig(os.path.join(chart_data_path, title_1 + '.png'))
 
 plt.show()
 
