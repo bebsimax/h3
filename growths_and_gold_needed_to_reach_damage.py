28fd6b8496f78d27732091e966f067034c5f0c3c
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-28 13:34:15 +0200

    produces stacked hbar chart, uses temp function for dmg calculation

--- growths_and_gold_needed_to_reach_damage.py
+++ growths_and_gold_needed_to_reach_damage.py
@@ -3,35 +3,32 @@ from common import average_damage
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import math
-chart_name = "base"
+
 set_damage = 500
-chart_path = os.path.join(chart_data_path, f"{chart_name}.png")
+title = f'number of maximum growths and gold needed to reach {set_damage} damage'
+chart_name = title
 states = ['upgraded', 'neutral']
 
 
-def set_average_damage(row):
-    if "twice" in row["special"]:
-        return average_damage.av_dmg(row["damage_min"], row["damage_max"], row["attack"]) * 2
-    else:
-        return average_damage.av_dmg(row["damage_min"], row["damage_max"], row["attack"])
+units_up_and_neu = units.query(f'state == "{states[0]}" or state == "{states[1]}"')
 
-units = units.query(f'state == "{states[0]}" or state == "{states[1]}"')
-units = units.assign(avg_dmg=units.apply(set_average_damage, axis=1))
-units["growths"] = (set_damage / units.avg_dmg / units.growth).round(0)
-units["gold_req"] = units.growths * units.gold_cost /1000
-print(units.head())
-#units = units.sort_values(by="average_damage", ascending=False)
+units_up_and_neu = units_up_and_neu.assign(avg_dmg=units_up_and_neu.apply(average_damage.avg_dmg_series, axis=1))
+units_up_and_neu["growths"] = (set_damage / units_up_and_neu.avg_dmg / units_up_and_neu.max_growth).round(0)
+units_up_and_neu["gold_req"] = units_up_and_neu.growths * units_up_and_neu.gold_cost / 1000
 
-units = units[["name", "fraction", "growths", "gold_req"]]
-w = 0.3
-units.sort_values('growths', ascending=True).plot(x=0, kind="barh",
+
+units_to_chart = units_up_and_neu[["name", "fraction", "growths", "gold_req"]]
+w = 0.6
+chart = units_to_chart.sort_values('growths', ascending=True).plot(x=0, kind="barh",
                                                   stacked=True,
                                                   figsize=(18, 10),
-                                                  width=0.8,
+                                                  width=w,
                                                   fontsize=6,
-                                                  ylabel="growths and gold in thousands",
-                                                  xticks=range(0, 115, 5),
-                                                  title=f"weeks of base growths and gold needed to reach {set_damage} damage")
+                                                  ylabel="growths, gold in thousands",
+                                                  xticks=range(0, int(max(units_up_and_neu["gold_req"])+max(units_up_and_neu["growths"])), 2),
+                                                  title=title)
+fig = chart.get_figure()
+fig.savefig(os.path.join(chart_data_path, chart_name + '.png'))
+
 
 plt.show()
