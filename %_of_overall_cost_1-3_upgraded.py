8b0b910990e347229ba9934e0a56199d9dd7a257
Jakub Podolski <60115575+bebsimax@users.noreply.github.com>
2021-05-28 09:14:17 +0200

    excluded corsairs in query, added path to save chart

--- %_of_overall_cost_1-3_upgraded.py
+++ %_of_overall_cost_1-3_upgraded.py
@@ -1,13 +1,18 @@
-from main import units, town_colors
+from main import units, town_colors, chart_data_path
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
+
 
 tiers = (1, 7)
 state = "upgraded"
-units = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]} & state == "{state}"')
-units["max_gold_cost"] = units.max_growth * units.gold_cost
+title = "'% of towns overall cost are units tiers 1-3'"
 
+
+
+units = units.query(f'tier >= {tiers[0]} & tier <= {tiers[1]} & state == "{state}" & name != "corsairs"')
+units["max_gold_cost"] = units.max_growth * units.gold_cost
 t = pd.DataFrame(index=[town for town in town_colors.keys() if town != 'neutral'])
 t["overall_gold_cost"] = units.groupby(["fraction"])['max_gold_cost'].sum()
 
@@ -20,9 +25,10 @@ t = t.sort_values(by="%", ascending=False)
 indexes = t.index.values
 colors_in_order = [town_colors[town] for town in indexes]
 
-t.plot(y=2, kind='bar', color=colors_in_order, figsize=(16.5, 8.5), xlabel="towns", ylabel="%", legend=False,
-       title='% of towns overall cost are units tiers 1-3', ylim=(20, 35))
-
+chart = t.plot(y=2, kind='bar', color=colors_in_order, figsize=(16.5, 8.5), xlabel="towns", ylabel="%", legend=False,
+       title=title, ylim=(20, 35))
+fig = chart.get_figure()
+fig.savefig(os.path.join(chart_data_path, title + '.png'))
 
 plt.show()
 
